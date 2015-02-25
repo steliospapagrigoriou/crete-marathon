24a9a25add455cebd466ebd24b5fcb43e060aa8f
Ben Jackson <benjackson84@gmail.com>
2015-02-25 12:01:09 +0000

    pulling temperature from forecast.io

--- js/crete.js
+++ js/crete.js
@@ -3,4 +3,19 @@ $(document).ready(function() {
     $(this).toggleClass('is-active');
     $('#top_signup_form').toggleClass('is-active');
   });
+  if($('#home_body').size() > 0){
+    // on the home page
+    $.ajax({
+      url: "https://api.forecast.io/forecast/36d5aba954e93fffa29e7df76465fe8b/35.34,25.14",
+      dataType: "jsonp",
+      data: "units=uk",
+      success: function (data) {
+        updateTemperature(data);
+      }
+    });
+  }
 });
+
+function updateTemperature(data){
+  $('#temperature_value').html(Math.ceil(data.currently.temperature)+'&deg;')
+};
